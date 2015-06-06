6885e0d473527288df6f61e2932033d425a14b92
Boynux <boynux@gmail.com>
2015-06-07 00:08:33 +0800

    Filered catregory list.
    To remove extra stuff from Json, 24 categories left.
    Sub-categories removed.

--- api/category.rb
+++ api/category.rb
@@ -5,22 +5,24 @@ require 'net/http'
 
 class Category < Grape::API
 
-prefix 'api'
+  prefix 'api'
 
-resource :healthcheck  do
-	get do
-	  "healthcheck good"		
-	end
-end
+  resource :healthcheck  do
+    get do
+      "healthcheck good"		
+    end
+  end
 
-resource :category do
-	get do
-		access_token = ENV['OAUTH_ACCESSTOKEN']
-		v_date = ENV['V']
-		uri = URI("https://api.foursquare.com/v2/venues/categories?oauth_token=#{access_token}&v=20150613")
-	    response = Net::HTTP.get(uri)
-	    JSON.parse(response)["response"]
-	end
-end
+  resource :category do
+    get do
+      access_token = ENV['OAUTH_ACCESSTOKEN']
+      v_date = ENV['V']
+      uri = URI("https://api.foursquare.com/v2/venues/categories?oauth_token=#{access_token}&v=20150613")
+      response = JSON.parse(Net::HTTP.get(uri))["response"]
 
+        response['categories']
+          .map { |c| {:id => c['id'], :name => c['name'], :pluralName => c['pluralName'], :shortName => c['shortName'] } }
+          .to_json
+    end
+  end
 end
